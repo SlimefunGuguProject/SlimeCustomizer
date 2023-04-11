ac96dfecc7df937b4a6300aa4a1b3df543322e5e
ybw0014 <i@guizhanss.com>
2023-04-11 04:19:24 -0400

    让看不懂英文的人能看懂（吧）的提示

--- src/main/java/io/ncbpfluffybear/slimecustomizer/SlimeCustomizer.java
+++ src/main/java/io/ncbpfluffybear/slimecustomizer/SlimeCustomizer.java
@@ -59,6 +59,13 @@ public class SlimeCustomizer extends JavaPlugin implements SlimefunAddon {
         instance = this;
         itemsFolder = new File(this.getDataFolder(), "saveditems");
 
+        if (!getServer().getPluginManager().isPluginEnabled("GuizhanLibPlugin")) {
+            getLogger().log(Level.SEVERE, "本插件需要 鬼斩前置库插件(GuizhanLibPlugin) 才能运行!");
+            getLogger().log(Level.SEVERE, "从此处下载: https://50l.cc/gzlib");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         // Read something from your config.yml
         Config cfg = new Config(this);
 
